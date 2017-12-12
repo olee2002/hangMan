1c6bae7154f7583f829d32c2db7a90195d0e870a
babylee2002 <babylee2002@gmail.com>
2017-12-12 14:41:22 -0500

    letter Removal function Working

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+
 //Creat an array of words that will be given to users to guess.
 const wordsArray = ['apple', 'banana', 'strawberry', 'watermelon', 'pineapple', 'orange', 'lemon', 'lime', 'kiwi', 'grape'];
 
@@ -21,38 +23,19 @@ let start = function () {
 $('#reset').on('click', start);
 
 
+
 //create the alphabet letters to choose in the game
-var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let letterChoice = str.split('')
-let iterateAlphabet = function() {
+let letterAlphabet =$('.Alphabet');
+for (let i = 0; i < letterAlphabet.length; i++) {
+    console.log(letterAlphabet[i])
+    letterAlphabet[i].addEventListener('click',letterClicked)
+    function letterClicked(){
+    this.innerHTML = "_";
+    }
+
     
-    for (var i = 0; i < letterChoice.length; i++) {
-    //console.log(letterChoice[i])
-    `<div class='letter'>A</div> `
- }
- $('#Alphabet').html(letterChoice)
 }
-iterateAlphabet();
-$('#Alphabet').on ('click', function(){
-this.target.letterChoice
-})
 
 
 
-// function letterChosen() {
-//     let letter = document.getElementById('EmptyLetters').value;
-//     if (letter.length > 0) {
-//         for (let i = 0; i < randomWord.length; i++) {
-//             if (randomWord[i] === letter) {
-//                 givenArray[i] = letter;
-//             }
-//         }
-//         count++;
-//         document.getElementById('TriesLeft').innerHTML = count - 1;
-//         document.getElementById('EmptyLetters').innerHTML = givenArray.join(' ')
-//     }
-//     if (count > 5) {
-//         alert("You Lost, Please Try Again!")
-//     }
-
-// }
+});
