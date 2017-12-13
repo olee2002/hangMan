0d8fece6cfd5daa1df1556582761e1616183d0f0
babylee2002 <babylee2002@gmail.com>
2017-12-13 07:22:40 -0500

    Added jQuery library

---
+++ HangMan_files/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+
+    //Creat an array of words that will be given to users to guess.
+    const wordsArray = ['apple', 'banana', 'strawberry', 'watermelon', 'pineapple', 'orange', 'lemon', 'lime', 'kiwi', 'grape'];
+
+
+
+    //Start - generate a random name of fruits from the given array and a corresponding number of blank spaces appears in the main playboard. 
+    let start = function () {
+        const randomWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+        console.log(randomWord)
+
+        let givenArray = [];
+        let answerArray = randomWord.split('');
+        //console.log(randomWord.split(''))
+        console.log(answerArray)
+        for (let i = 0; i < randomWord.length; i++) {
+            givenArray[i] = '_';
+        }
+        $('#EmptyLetters').html(givenArray.join(' '));
+
+        //create the alphabet letters and disappear when clicked in the game
+        let letterAlphabet = $('.Alphabet');
+        let count = 8;
+        let letterBank = [];
+
+
+        letterAlphabet.on('click', function () {
+            let letterInput = this.innerHTML.toLowerCase();
+            let letterOutput = givenArray
+            let indexOfLetter = answerArray.indexOf(letterInput)
+            if (indexOfLetter === -1) {
+                //msg when fail
+                alert('try again');
+                //Remaining tries counter
+                count -= 1;
+                $('#TriesLeft').html(count);
+                letterBank.push(letterInput)
+                $('#WrongLetters').html(letterBank)
+                //Adding wrong letter to the letter bank
+                console.log(letterBank.join(' '))
+                this.innerHTML = '_';
+
+            } else {
+                //msg when succeeds
+                alert("Yay You've got it")
+                //Add letter to the correspoinding empty space
+                letterOutput[indexOfLetter] = letterInput
+                $('#EmptyLetters').html(letterOutput.join(' ').toUpperCase());
+
+            }
+
+        })
+    }
+    //start();
+    $('#reset').on('click', start);
+
+
+
+
+
+
+
+});
