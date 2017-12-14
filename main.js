d72f3f75a35674fdfb3a00b9db361a9a1eca5779
babylee2002 <babylee2002@gmail.com>
2017-12-14 11:50:51 -0500

    Added comment for GameOver

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
             let indexOfLetter = answerArray.indexOf(letterInput)
 
             function endingEffect() {
-                $('#reset').html('Game Over!');
+                $('#reset').html('Game Over! Click the Heart Below to Restart. &#8595;');
                 $('#StarOver').attr({ class: "animated tada" });
 
             }
