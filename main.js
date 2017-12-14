c9fe76e83f55221d64a3b35cf978855e19ea0613
babylee2002 <babylee2002@gmail.com>
2017-12-14 10:43:06 -0500

    Page Refresh button Added

--- main.js
+++ main.js
@@ -35,23 +35,36 @@ $(document).ready(function () {
                 count -= 1;
                 if (count > 0) {
                     $('#TriesLeft').html(count);
-                    letterBank.push(letterInput)
-                    $('#WrongLetters').text(letterBank.join(' '))
+                    letterBank.push(letterInput);
+                    $('#WrongLetters').text(letterBank.join(' '));
                     this.innerHTML = '_';
 
                 } else if (count === 0) {
-                    alert('You Lost! Please try again')
+                    alert('You Lost! Please try again');
+                    $('#TriesLeft').html(count);
+                    $('#reset').html('Game Over!');
+                    $('#StarOver').attr({
+                        class: "animated infinite tada"
+                    });
+                    $('#StarOver').html('Click Me!')
+
+
+
                 }
             } else {
                 answerArray.forEach(function (item, index) {
                     if (letterInput === item) {
-                        givenArray[index] = letterInput
+                        givenArray[index] = letterInput;
+                        $(event.target).css('color', 'rgba(0,0,0,0.2)');
 
                     }
                 })
                 $('#EmptyLetters').html(givenArray.join(' '));
                 if (givenArray.join('') === answerArray.join('')) {
                     alert('You Win!')
+                    $('#reset').text('Game Over!')
+                    $('#StarOver').html('Click Me!')
+
                 }
 
 
@@ -63,8 +76,12 @@ $(document).ready(function () {
 
     //start();
     $('#reset').on('click', start);
+    $('#heart').on('click', myFunction);
 
-    const audio = document.getElementById("audio");
+    function myFunction() {
+        location.reload();
+    };
+    const audio = $("audio");
     audio.volume = 0.4;
 
 
